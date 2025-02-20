4b54f0bcfdcf0e332f592e293bf53e0620d16cf6
Mari1916 <mariana.freitas1916@gmail.com>
2025-02-20 10:34:58 -0300

    Adicionando funcionalidade de limpar campos

--- 20-02/script.js
+++ 20-02/script.js
@@ -1,7 +1,23 @@
-const nome = prompt("Digite seu nome");
-const idade = prompt("Digite sua idade");
+//criando as variáveis 
+const nome = document.getElementById("nome")
+const cidade = document.getElementById("cidade")
+const resultado = document.getElementById("resultado")
+const resultadoCid = document.getElementById("resultadoCid")
 
+//criando função 
+function limparCampo() {
+    nome.value = ""
+    cidade.value = ""
+}
+function mudar(event) {
+    event.preventDefault()
+    console.log(nome.value)
+    console.log(cidade.value)
 
+    //imprimir o nome na tela
+    resultado.innerHTML = nome.value
+    resultadoCid.innerHTML = cidade.value
 
-//console.log("Meu nome é: " + nome + "E minha idade é: " + idade);
-console.log(`Meu nome é: ${nome} E minha idade é: ${idade} Anos`);
+    //Limpando campos
+    limparCampo()
+}
